c3f7a9c0287cc6bac4d77a0e45ff96f3eb705893
Alexander Borodin <98040616+Borodin1@users.noreply.github.com>
2024-06-14 21:00:46 +0300

    feat: add settings auth

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 // import { GetTaskWithFilter } from './dto/get-task-filter.dto';
 // import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskWithFilter } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
@@ -12,11 +13,18 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
+  @Get()
+  getTask(@Query() filterDto: GetTaskWithFilter): Promise<Task[]> {
+    return this.taskService.getTasks(filterDto);
+  }
+
   @Get('/:id')
   getTaskById(@Param('id') id: string): Promise<Task> {
     return this.taskService.getTaskById(id);
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,6 +7,7 @@ import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { Repository } from 'typeorm';
+import { GetTaskWithFilter } from './dto/get-task-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -15,6 +16,24 @@ export class TasksService {
     private tasksRepository: Repository<Task>,
   ) {
     console.log('TasksRepository injected:', !!this.tasksRepository);
+  }
+
+  async getTasks(filterDto: GetTaskWithFilter): Promise<Task[]> {
+    const { search, status } = filterDto;
+    const query = this.tasksRepository.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
   }
 
   async getTaskById(id: string): Promise<Task> {
